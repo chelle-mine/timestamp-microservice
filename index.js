5ff5767bd4da2ee3fa14f97fded7eb6ea4155bc9
chelle-mine <mhkshin@gmail.com>
2017-12-01 15:25:31 -0500

    convert dates to UTC

--- index.js
+++ index.js
@@ -12,22 +12,35 @@ app.get('/', (req, res) => res.render('index', {
 }));
 
 app.param('dateIn', (req, res, next, dateIn) => {
+	// object properties null by default
 	req.date = {
 		unix: null,
 		natural: null
 	};
+
 	// if `dateIn` is an int, presume UNIX timestamp
 	if (Number.isInteger(parseInt(dateIn))) {
-		dateIn = new Date(parseInt(dateIn) * 1000).toString();
+		const d = new Date(parseInt(dateIn) * 1000);
+
+		if (!isNaN(d.getTime())) {
+			// convert to UTC for consistency
+			const utc = new Date(d.getUTCFullYear(), d.getUTCMonth(), d.getUTCDate(), d.getUTCHours());
+
+			req.date.unix = d.getTime() / 1000;
+			req.date.natural = utc.toLocaleString('en-us', {month: 'long', day: '2-digit', year: 'numeric'});
+		}
 	}
 
 	// see if chrono-node can parse dateIn
-	if (chrono.parseDate(dateIn)) {
-		// pass in parsed date to a new Date obj
-		const d = new Date(chrono.parseDate(dateIn));
-		// assign to properties in req obj
-		req.date.unix = d.getTime() / 1000;
-		req.date.natural = d.toDateString();
+	else if (chrono.parseDate(dateIn)) {
+		// remove timezone offset
+		let result = chrono.parse(dateIn)[0];
+		result.start.assign('timezoneOffset', 0);
+
+		const d = result.start.date();
+		
+		req.date.unix = Date.UTC(d.getFullYear(), d.getMonth(), d.getDate()) / 1000;
+		req.date.natural = d.toLocaleString('en-us', {month: 'long', day: '2-digit', year: 'numeric'});
 	}
 	
 	next();
