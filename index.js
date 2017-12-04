7429858efddf22f0598595544839cc38081d0496
chelle-mine <mhkshin@gmail.com>
2017-12-04 15:41:55 -0500

    add PORT env

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const chrono = require('chrono-node');
 const express = require('express');
+const path = require('path');
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 // Set view
-app.set('views', __dirname + '/views');
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.get('/', (req, res) => res.render('home', {
@@ -50,4 +52,4 @@ app.use('/:dateIn', (req, res) => {
     res.send(JSON.stringify(req.date));
 });
 
-app.listen(3000, () => console.log('Listening on port 3000...'));
+app.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
